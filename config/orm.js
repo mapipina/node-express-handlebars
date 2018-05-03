97ffe45c6970c87d1c6901cffb9bcfcb0f6dacf0
Mapi Pina <mpina09@gmail.com>
2018-05-03 14:06:04 -0400

    setting up additional folders

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,28 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-  selectAll: function(tableInput) {
+  selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM ??";
 
     connection.query(queryString, [tableInput], function(err, result) {
       // if (err) throw err;
-      return result;
+      cb(result);
     });
   },
-  insertOne: function(tableInput, colToSearch, valOfCol) {
+  insertOne: function(tableInput, colToSearch, valOfCol, cb) {
     var queryString = "INSERT INTO ?? (??) VALUES (?)";
 
     connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
       // if (err) throw err;
-      return result;
+      cb(result);
     });
   },
-  updateOne: function(tableInput, colToSearch, valOfCol, secondCol, secondVal) {
+  updateOne: function(tableInput, colToSearch, valOfCol, secondCol, secondVal, cb) {
     var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
 
     connection.query(queryString, [tableInput, colToSearch, valOfCol, secondCol, secondVal], function(err, result) {
       // if (err) throw err;
-      return result;
+      cb(result);
     });
   }
 };
